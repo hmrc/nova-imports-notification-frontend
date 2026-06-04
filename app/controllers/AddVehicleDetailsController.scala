263bb04a265a7720367539bb56d386ec16b7bb6d
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2026-06-04 11:36:49 +0100

    Adding screen and logic + tests

---
+++ app/controllers/AddVehicleDetailsController.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import controllers.actions.*
+import forms.AddVehicleDetailsFormProvider
+
+import javax.inject.Inject
+import models.{AddVehicleDetails, Mode, NovaUserType, UserAnswers}
+import navigation.Navigator
+import pages.{AddVehicleDetailsPage, VehicleFromEuPage}
+import play.api.data.Form
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import views.html.AddVehicleDetailsView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AddVehicleDetailsController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  actions: Actions,
+  formProvider: AddVehicleDetailsFormProvider,
+  view: AddVehicleDetailsView,
+  appConfig: FrontendAppConfig
+)(implicit ec: ExecutionContext)
+    extends BaseController {
+
+  val form: Form[AddVehicleDetails] = formProvider()
+
+  private val guardPredicate: UserAnswers => Boolean =
+    _.get(VehicleFromEuPage).contains(true)
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = actions.vatTraderAuthAndGetDataWithGuard(guardPredicate) { implicit request =>
+    Ok(view(form.withDefault(request.userAnswers.get(AddVehicleDetailsPage)), mode, appConfig.multipleVehiclesSpreadsheetsUrl))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = actions.vatTraderAuthAndGetDataWithGuard(guardPredicate).async { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, appConfig.multipleVehiclesSpreadsheetsUrl))),
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(AddVehicleDetailsPage, value))
+            _              <- sessionRepository.set(updatedAnswers)
+          } yield Redirect(
+            navigator.nextPage(AddVehicleDetailsPage, mode, updatedAnswers, NovaUserType.from(request.affinityGroup, request.enrolments))
+          )
+      )
+  }
+}
