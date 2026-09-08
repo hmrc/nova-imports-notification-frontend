80bbd91eec43b7f933f36e636ed397755afc3297
Simon Hayden <244017492+simonhdw@users.noreply.github.com>
2026-09-08 15:59:29 +0100

    [DTR-7867][DTR-7868][DTR-7869] UVS-3.0,3.1,3.2 error screens (#186)

---
+++ app/controllers/vehicledetails/UploadSpreadsheetErrorRejectedController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.vehicledetails
+
+import controllers.BaseController
+import controllers.actions.Actions
+import controllers.utils.IsDraftIdDefined
+import models.requests.DataRequest
+import pages.sections.initialquestions.VehicleFromEuPage
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import views.html.UploadSpreadsheetErrorRejectedView
+
+import javax.inject.Inject
+
+class UploadSpreadsheetErrorRejectedController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  actions: Actions,
+  view: UploadSpreadsheetErrorRejectedView
+) extends BaseController {
+
+  import UploadSpreadsheetErrorRejectedController.*
+
+  def onPageLoad(): Action[AnyContent] = actions.authAndGetDataWithUserTypeGuard(guardPredicate) { implicit request =>
+    Ok(view())
+  }
+}
+
+object UploadSpreadsheetErrorRejectedController {
+
+  def guardPredicate(request: DataRequest[?]): Boolean =
+    IsDraftIdDefined(request.userAnswers) &&
+      request.userAnswers.get(VehicleFromEuPage).isDefined &&
+      (request.userContext.isVatRegisteredOrganisation || request.userContext.isAgentWithClient)
+}
