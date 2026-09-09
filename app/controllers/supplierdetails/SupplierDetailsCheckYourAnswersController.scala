cf73159f33bbce81460195e3db259746c24c4ceb
alexdeacy-hmrc <307525768+alexdeacy-hmrc@users.noreply.github.com>
2026-09-09 16:30:20 +0100

    DTR-6539_2 Updated controller data guard to ensure all previous answers are given before allowing the user to access the CYA screen.

--- app/controllers/supplierdetails/SupplierDetailsCheckYourAnswersController.scala
+++ app/controllers/supplierdetails/SupplierDetailsCheckYourAnswersController.scala
@@ -20,12 +20,13 @@ import connectors.NovaImportsBackendConnector
 import controllers.BaseController
 import controllers.actions.*
 import controllers.utils.IsDraftIdDefined
+import models.BusinessOrPrivateIndividual.{Business, PrivateIndividual}
 import models.draftsections.{SupplierDetails, SupplierSelfSupplyDetails}
 import models.requests.DataRequest
 import models.{Address, BusinessOrPrivateIndividual, NameDetails, NormalMode, SupplierNumber, UserAnswers, VatNumberDetails}
 import pages.*
 import pages.sections.initialquestions.VehicleFromEuPage
-import pages.sections.supplieraddress.{SupplierAddressJourneyIdPage, SupplierAddressPage}
+import pages.sections.supplieraddress.{IsSupplierAddressInTheUkPage, SupplierAddressJourneyIdPage, SupplierAddressPage}
 import pages.sections.supplierdetails.*
 import play.api.Logging
 import play.api.libs.json.{JsObject, Json}
@@ -152,11 +153,34 @@ object SupplierDetailsCheckYourAnswersController {
         || answers.get(UsePurchaserDetailsAsSupplierPage(supplierNumber)).isDefined
 //      || answers.get(UseClientDetailsAsSupplierPage(supplierNumber)).isDefined   TODO: include this line once AVD-S1.2 page is added
 
+    val supplierDetailsQuestionsAnswered = if (answers.get(UsePersonalDetailsAsSupplierPage(supplierNumber)).contains(false)
+      || answers.get(UsePurchaserDetailsAsSupplierPage(supplierNumber)).contains(false)) {// TODO: include this line once AVD-S1.2 page is added
+      supplierDetailsDefined(answers, supplierNumber)
+    } else {
+      true
+    }
+
     IsDraftIdDefined(answers)
-    && avdQuestionAnswered
-    && request.userAnswers.get(VehicleFromEuPage).contains(true)
-    && supplierService.numberExists(request.userAnswers, supplierNumber)
+      && supplierDetailsQuestionsAnswered
+      && avdQuestionAnswered
+      && request.userAnswers.get(VehicleFromEuPage).contains(true)
+      && supplierService.numberExists(request.userAnswers, supplierNumber)
   }
+
+
+  private def supplierDetailsDefined(answers: UserAnswers, supplierNumber: SupplierNumber): Boolean = {
+    answers.get(SupplierBusinessOrIndividualPage(supplierNumber)).isDefined
+      && (answers.get(SupplierBusinessOrIndividualPage(supplierNumber)).contains(PrivateIndividual)
+          || answers.get(SupplierBusinessNamePage(supplierNumber)).isDefined)
+      && (answers.get(SupplierBusinessOrIndividualPage(supplierNumber)).contains(Business)
+          || answers.get(SupplierNamePage(supplierNumber)).isDefined)
+      && answers.get(IsSupplierAddressInTheUkPage(supplierNumber)).isDefined
+      && answers.get(SupplierAddressPage(supplierNumber)).isDefined
+      && answers.get(IsSupplierVatRegisteredPage(supplierNumber)).isDefined
+      && (answers.get(IsSupplierVatRegisteredPage(supplierNumber)).contains(false)
+          || answers.get(SupplierVatRegistrationNumberPage(supplierNumber)).isDefined)
+  }
+
 
   private def isSelfSupply(answers: UserAnswers, supplierNumber: SupplierNumber): Boolean = {
     def isTrue(page: QuestionPage[Boolean]): Boolean = answers.get(page).contains(true)
